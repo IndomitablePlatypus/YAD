c858ce0635b3f5e2445390329928399c86996c2d
IndomitablePlatypus <arutomi@gmail.com>
2015-09-24 12:12:51 +0300

    Bugfix and some inline documentation

--- yad.js
+++ yad.js
@@ -23,37 +23,215 @@ function iselse(variable, value) {
     return value;
 }
 
-YAD = {
+/**
+ * Yet Another event Dispatcher
+ * 
+ * @namespace
+ * @type YAD
+ */
+var YAD = {
+    /**
+     * Dispatcher singleton
+     * 
+     * @private
+     * @property
+     * @type {YAD.Dispatcher}
+     */
     _dispatcher: undefined,
     /**
+     * Returns <b>YAD.Dispatcher</b> singleton in <b>YAD</b> namespace
      * 
+     * @method
+     * @public
      * @returns {YAD.Dispatcher}
      */
     getDispatcher: function () {
         return thereis(this._dispatcher) ? this._dispatcher : this._dispatcher = new this.Dispatcher();
     },
+    /**
+     * Creates new <b>YAD.Event</b> object
+     * 
+     * @public
+     * @method
+     * @param {String} name Event name (identifier)
+     * @param {Object|null} data Basically any data associated with the event
+     * @param {Function|null} closure Closure for the event. 
+     *                                Closure will be executed after all relevant 
+     *                                listeners are notified of the event.
+     * @returns {YAD.Event} New <b>Event</b> object
+     */
     createEvent: function (name, data, closure) {
         return new this.Event(name, data, closure);
     },
+    /**
+     * Creates new <b>YAD.Listener</b> object
+     * 
+     * @public
+     * @method
+     * @param {Function} executor The listener itself 
+     *                            (name of some function, method, closure itself).
+     * @param {Object|null} context Context for the executor to be called in 
+     *                            (basically what would <b>this</b> mean inside executor).
+     *                            <b>window</b> object by default.
+     * @param {String|null} name Name assigned to the listener. This name can be 
+     *                            further used when running <b>YAD.unlisten...</b>
+     *                            methods for example.
+     *                            <b>executor.toString()</b> by default.
+     * @returns {YAD.Listener} New <b>Listener</b> object
+     */
     createListener: function (executor, context, name) {
         return new this.Listener(executor, context, name);
     },
+    /**
+     * Declaration of <b>YAD.Dispatcher</b> class and method that returns
+     * YAD.Dispatcher singleton in YAD namespace at the same time. <br /> 
+     * When used as <b>YAD.Dispatcher()</b> call it will return YAD._dispatcher singleton. <br />
+     * When used as <b>new YAD.Dispatcher()</b> it will return entirely new Dispatcher
+     * object for you to (<i>ab</i>)use as you wish.
+     * 
+     * @class
+     * @returns {YAD.Dispatcher} New <b>Dispatcher</b> object OR <b>YAD._dispatcher</b> singleton
+     */
     Dispatcher: function () {
         return this instanceof YAD.Dispatcher ? this : this.getDispatcher();
     },
+    /**
+     * Declaration of YAD.Event calss. Use <b>YAD.createEvent</b> instead.
+     * 
+     * @class
+     * @param {String} name Event name (identifier)
+     * @param {Object|null} data Basically any data associated with the event
+     * @param {Function|null} closure Closure for the event. 
+     *                                Closure will be executed after all relevant 
+     *                                listeners are notified of the event.
+     * @returns {YAD.Event}
+     */
     Event: function (name, data, closure) {
-        // "this" is YE, when calling new YAD.Event
+        // "this" is YAD.Event, when calling new YAD.Event
         this._parse(name, data, closure);
     },
+    /**
+     * Declaration of YAD.Listener class. Use <b>YAD.createListener</b> instead.
+     * 
+     * @class
+     * @param {Function} executor The listener itself 
+     *                            (name of some function, method, closure itself).
+     * @param {Object|null} context Context for the executor to be called in 
+     *                            (basically what would <b>this</b> mean inside executor).
+     *                            <b>window</b> object by default.
+     * @param {String|null} name Name assigned to the listener. This name can be 
+     *                            further used when running <b>YAD.unlisten...</b>
+     *                            methods for example.
+     *                            <b>executor.toString()</b> by default.
+     * @returns {YAD.Listener}
+     */
     Listener: function (name, executor, context) {
+        // "this" is YAD.Listener, when calling new YAD.Listener
         this._parse(name, executor, context);
     },
+    /**
+     * Alias for <b>YAD.getDispatcher().say</b><br />
+     * Dispatches event (creating it if necessary).
+     * All listeners are notified immediately.
+     * 
+     * @public
+     * @method
+     * @param {YAD.Event|String} event Either single <b>YAD.Event</b> instance
+     *                                 or string to be used as Event name. 
+     *                                 If string contains <b>"."</b> symbols,
+     *                                 several events will be fired. <br />
+     *                                 For example: <br />
+     *                                 <b>log.someaction.success</b> will be dispatched
+     *                                 to all listeners of <b>log</b> event, 
+     *                                 all listeners of <b>log.someaction</b> event and 
+     *                                 all listeners of <b>log.someaction.success<b> event.
+     *                                 Each event will contain <b>data</b> object, but closure 
+     *                                 will be called only once still.
+     * @param {Object|null} data Basically any data associated with the event.
+     *                                 If <b>event</b> is YAD.Event instance, and this param 
+     *                                 is used, event.data will be rewritten with this param value.
+     *                                 Pass <b>undefined</b> if you want to omit this param yet 
+     *                                 use <b>closure</b>.
+     * @param {Function|null} closure Closure for the event. 
+     *                                 Closure will be executed after all relevant 
+     *                                 listeners are notified of the event.
+     *                                 Closure should accept array of Event objects, event if 
+     *                                 Event instance is passed as the first param (in that case 
+     *                                 array will contain one element).
+     * @returns {true|mixed} True if there is no <b>closure</b>, results of <b>closure</b> 
+     *                                 execution otherwise.
+     */
     yell: function (event, data, closure) {
         return this.getDispatcher().say(event, data, closure);
     },
+    /**
+     * Alias for <b>YAD.getDispatcher().say</b>.<br />
+     * Dispatches event (creating it if necessary).
+     * All listeners are notified using setTimeout, thus after caller code block is finished.
+     * Timeout delay can be configured using <b>YAD.getDispatcher().configure</b> method. 
+     * Default is 0.
+     * 
+     * @public
+     * @method
+     * @param {YAD.Event|String} event Either single <b>YAD.Event</b> instance
+     *                                 or string to be used as Event name. 
+     *                                 If string contains <b>"."</b> symbols,
+     *                                 several events will be fired. <br />
+     *                                 For example: <br />
+     *                                 <b>log.someaction.success</b> will be dispatched
+     *                                 to all listeners of <b>log</b> event, 
+     *                                 all listeners of <b>log.someaction</b> event and 
+     *                                 all listeners of <b>log.someaction.success<b> event
+     * @param {Object|null} data Basically any data associated with the event.
+     *                                 If <b>event</b> is YAD.Event instance, and this param 
+     *                                 is used, event.data will be rewritten with this param value.
+     *                                 Pass <b>undefined</b> if you want to omit this param yet 
+     *                                 use <b>closure</b>.
+     * @param {Function|null} closure Closure for the event. 
+     *                                 Closure will be executed after all relevant 
+     *                                 listeners are notified of the event.
+     *                                 Closure should accept array of Event objects, event if 
+     *                                 Event instance is passed as the first param (in that case 
+     *                                 array will contain one element).
+     * @returns {true|mixed} True if there is no <b>closure</b>, results of <b>closure</b> 
+     *                                 execution otherwise. <br />
+     *                                 <b>Note</b>: in the case of whisper closure 
+     *                                 is executed after the timeout is set, but before 
+     *                                 listeners themselves are executed.
+     */
     whisper: function (event, data, closure) {
         return this.getDispatcher().say(event, data, closure, false);
     },
+    /**
+     * Alias for <b>YAD.getDispatcher().listen</b>.<br />
+     * Register one or more listeners to be notified when the event occures.
+     * 
+     * @public
+     * @method
+     * @param {YAD.Event|string} event Either single <b>YAD.Event</b> instance
+     *                                 or string to be used as Event name. 
+     *                                 Listeners will be registered to this particular 
+     *                                 event without regard to the event name composition
+     *                                 (using "." or no).
+     * @param {Function} listener Function name, callable variable, method, closure 
+     *                                 - whatever you  want to be executed when 
+     *                                 event is fired. <br />
+     *                                 You can use prepared <b>YAD.Listener</b> object. 
+     *                                 In this case there is no need to pass any context 
+     *                                 (it will be ignored). <br />
+     *                                 Also you can use array to pass several listeners.
+     *                                 Each item can be either listener or <b>YAD.Listener</b> 
+     *                                 object or array of two elemnts where the first 
+     *                                 one is listener, and the second one is context.
+     *                                 Context param will be used with any 
+     *                                 listeners that don't have their own.
+     * @param {Object|null} context If you rely on <b>"this"</b> keyword inside the listener you can
+     *                                 still safely ise it. Just pass what exactly should be 
+     *                                 considered as "this".
+     * @returns {YAD.Dispatcher} YAD Dispatcher singleton. As there is no distinction 
+     *                                 between YAD.listen and Dispatcher.listen calls, 
+     *                                 you can chain listener registraion
+     */
     listen: function (event, listener, context) {
         return this.getDispatcher().listen(event, listener, context);
     }
@@ -155,9 +333,9 @@ YAD.Dispatcher.prototype = {
         if (thereis(this._listeners[listenerName])) {
             delete this._listeners[listenerName];
         }
-        for(var eventName in this._eventListeners) {
-            if(this._eventListeners.hasOwnProperty(eventName)) {
-                if(thereis(this._eventListeners[eventName][listenerName])) {
+        for (var eventName in this._eventListeners) {
+            if (this._eventListeners.hasOwnProperty(eventName)) {
+                if (thereis(this._eventListeners[eventName][listenerName])) {
                     delete this._eventListeners[eventName][listenerName];
                 }
             }
@@ -165,7 +343,7 @@ YAD.Dispatcher.prototype = {
         return this;
     },
     _removeEvent: function () {
-        
+
     },
     say: function (event, data, closure, immediate) {
         if (!thereis(event)) {
@@ -180,7 +358,7 @@ YAD.Dispatcher.prototype = {
                 this._dispatch(events);
             }.bind(this), this._delay, events);
         }
-        return thereis(closure) ? closure() : true;
+        return thereis(closure) ? closure(events) : true;
     },
     listen: function (event, listener, context) {
         var events = this._prepareEvents(event);
@@ -199,16 +377,16 @@ YAD.Dispatcher.prototype = {
     },
     unlistenEvent: function (event) {
         var events = this._prepareEvents(event);
-        if(thereis(this._eventListeners[events[0].getName()])) {
+        if (thereis(this._eventListeners[events[0].getName()])) {
             delete this._eventListeners[events[0].getName()];
         }
         return this;
     },
     unlistenEventHeap: function (event) {
         var events = this._prepareEvents(event);
-        for(var property in this._eventListeners) {
-            if(this._eventListeners.hasOwnProperty(property)) {
-                if(property.indexOf(events[0].getName()) === 0) {
+        for (var property in this._eventListeners) {
+            if (this._eventListeners.hasOwnProperty(property)) {
+                if (property.indexOf(events[0].getName()) === 0) {
                     delete this._eventListeners[property];
                 }
             }
